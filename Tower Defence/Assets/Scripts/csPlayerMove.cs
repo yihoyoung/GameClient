beca091fbce35f93c74fb977f74748da6c250545
yihoyoung <yihoyoung@nate.com>
2015-03-21 17:50:51 +0900

    first commit

---
+++ Tower Defence/Assets/Scripts/csPlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csPlayerMove : MonoBehaviour {
+
+	Vector3 TargetPosition, playerDestination;
+	NavMeshAgent playerAgent;
+	RaycastHit hit;
+	Ray ray;
+	// Use this for initialization
+	void Start () {
+		playerAgent = GetComponent<NavMeshAgent> ();
+		playerDestination = playerAgent.destination;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.transform.tag == "Plane"){
+					playerAgent.SetDestination(hit.point);
+				}
+			}
+		}
+	}
+}
